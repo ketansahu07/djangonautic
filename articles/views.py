4e1d32461f6ff747a3777fb58dc86666b35e37a1
Ketan Sahu <ketankumar120@gmail.com>
2020-06-07 16:10:17 +0530

    logout system added

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Articles
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
 def article_list(request):
 	articles = Articles.objects.all().order_by('date')
 	return render(request, 'articles/article_list.html', {'articles': articles})
@@ -10,3 +12,7 @@ def article_detail(request, slug):
 	# return HttpResponse(slug)
 	article = Articles.objects.get(slug=slug)
 	return render(request, 'articles/article_detail.html', { 'article':article })
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+	return render(request, 'articles/article_create.html')
